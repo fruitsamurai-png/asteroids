da6adfc76008cbd5dbe2d31415055099fd1f1a6a
fruitsamurai-png <64308011+fruitsamurai-png@users.noreply.github.com>
2021-02-09 22:35:16 +0800

    Update GameState_Asteroids.cpp

--- CSD1130_Asteroids/CSD1130_Asteroids/Src/GameState_Asteroids.cpp
+++ CSD1130_Asteroids/CSD1130_Asteroids/Src/GameState_Asteroids.cpp
@@ -572,7 +572,6 @@ void GameStateAsteroidsDraw(void)
 		onValueChange = false;
 	}
 	AEGfxPrint(Text, strBuffer, 0.0f, 0.0f, 2.0f, 1.0f, 1.0f, 1.0f);
-
 }
 
 /******************************************************************************/
